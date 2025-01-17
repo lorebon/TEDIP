5b5804b755f8ec5b576e7fdd8dceb510759fbc43
lorebon <95094358+lorebon@users.noreply.github.com>
2025-01-17 12:48:58 +0100

    Update experiments_noval.py

--- temporal-RL/experiments_noval.py
+++ temporal-RL/experiments_noval.py
@@ -4,8 +4,7 @@ import time
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
-from ColumnGeneration_tree import *
-from fidelityMeasure import regressionDisagreement, F1ComplCorr
+from GenerateProblem import *
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.preprocessing import StandardScaler
 from dataset import load_openml
@@ -20,7 +19,7 @@ from preprocessing import preprocessTrain, preprocessTest
 def computeResults(dataset, minshap, maxshap, n_shap, seed):
     np.random.seed(seed)
 
-    columns = ['Seed', 'Represented trees', 'Represented paths', 'Disagreement', 'Leaves', 'Accuracy', 'Full Model', 'Best tree disag.', 'Best tree Accuracy']
+    columns = ['Seed', 'Represented trees', 'Represented paths', 'Leaves', 'Accuracy', 'Full Model']
 
     ### Load data
     X_train, y_train = load_UCR_UEA_dataset(name=dataset, split='Train', return_X_y=True)
@@ -39,51 +38,14 @@ def computeResults(dataset, minshap, maxshap, n_shap, seed):
     else:
         n_shap = int(n_shap)
 
-    if n_shap < 100:
-        n_shap = 100
-
     leaf_nodes = 6
 
-    reptrees, reprpaths, paths, labels, clf, acc, og_score, best_tree_disag, best_tree_acc = computeAll(X_train, y_train, X_test, y_test, depth, seed, minshap, maxshap, n_shap, Nmin=None, leaf_nodes=leaf_nodes)
-    disag = regressionDisagreement(X_test, y_test, clf, paths, labels)
-    #f1_score = F1ComplCorr(clf, paths)
+    reptrees, reprpaths, paths, labels, clf, acc, og_score = computeAll(X_train, y_train, X_test, y_test, depth, seed, minshap, maxshap, n_shap, Nmin=None, leaf_nodes=leaf_nodes)
     print("accuracy:", acc)
-    data = [seed, reptrees, reprpaths, disag, len(paths), acc, og_score, best_tree_disag, best_tree_acc]
+    data = [seed, reptrees, reprpaths, disag, len(paths), acc, og_score]
     df = pd.DataFrame([data], columns=columns)
 
     return df
-
-    timeseries = X_train[3, :]
-    shapelet = paths[0][0][0]
-    shapelet_len = len(shapelet)
-    min_distance = 100
-
-
-    for offset in range(len(timeseries) - shapelet_len + 1):
-        subsequence = timeseries[offset:offset + shapelet_len]
-        # Compute Euclidean distance between subsequence and shapelet
-        distance = np.sqrt(np.sum((subsequence - shapelet) ** 2))
-
-        if distance < min_distance:
-            min_distance = distance
-            best_offset = offset
-
-    # Output the result
-    print(f"Minimum Euclidean distance: {min_distance}")
-    print(f"Best offset (index of best match): {best_offset}")
-
-    exit(2)
-
-    # Save timeseries data to 'timeseries.txt'
-    with open('timeseries_2.txt', 'w') as f:
-        for idx, value in enumerate(timeseries):
-            f.write(f"{idx} {value}\n")
-
-    # Save shapelet data to 'shapelet.txt'
-    with open('shapelet_2.txt', 'w') as f:
-        for idx, value in enumerate(shapelet):
-            f.write(f"{idx+best_offset} {value}\n")
-
 
 if __name__ == '__main__':
     # parse dataset name
@@ -97,11 +59,7 @@ if __name__ == '__main__':
     minshap = args.l
     maxshap = args.u
     n_shap = args.r
-
-    test = computeResults(dataset, minshap, maxshap, n_shap, 6)
-    #print(test)
-    exit(2)
-
+    
     # parallelize over different seeds
     seeds = list(range(10))
     pool = mp.Pool(10)
