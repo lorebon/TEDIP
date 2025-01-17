309fddf43d3aea246a530f7bf2c1d0c58d1db175
lorebon <95094358+lorebon@users.noreply.github.com>
2025-01-17 12:01:08 +0100

    Add files via upload

---
+++ temporal-RL/mip.py
@@ -0,0 +1,24 @@
+from gurobipy import *
+import numpy as np
+
+
+def generateCSP(L, n, A):
+    m = Model('Integer MP')
+    #m.setParam('OutputFlag', 0)
+    z = m.addVars(L, vtype=GRB.BINARY, name="z")
+    m.addConstrs((quicksum(A[i, j] * z[j] for j in range(L)) == 1 for i in range(n)), name='con1')
+    #m.update()
+    return m, z
+
+
+def generateProblemSoft(L, n, A, l, loss, freq, lambd):
+    m = Model('Integer MP')
+    m.setParam('OutputFlag', 0)
+    z = m.addVars(L, vtype=GRB.BINARY, name="z")
+    m.addConstrs((quicksum(A[i, j] * z[j] for j in range(L)) == 1 for i in range(n)), name='con1')
+    m.setObjective(quicksum(- (1 - lambd) * loss[j] * z[j] for j in range(L)) + lambd*quicksum(freq[j] * z[j] for j in range(L)),
+                               sense=GRB.MAXIMIZE)
+
+    m.addConstr(sum(z.select('*')) <= l, name='card')
+    m.update()
+    return m, z
